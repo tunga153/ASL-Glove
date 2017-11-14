b148ffbb3063362223d03e138d8ea72d043ce02f
Brian Amaratunga <a3kila@gmail.com>
2017-11-14 14:39:41 -0500

    Correct file

---
+++ bluetooth_echo/bluetooth_echo.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(4, 2);
+
+String message; //string that stores the incoming message
+
+void setup()
+{
+  Serial.begin(9600); //set baud rate
+}
+
+void loop()
+{
+  /*
+  while(Serial.available())
+  {//while there is data available on the serial monitor
+    message+=char(Serial.read());//store string from serial command
+  }
+  if(!Serial.available())
+  {
+    if(message!="")
+    {//if data is available
+      Serial.println(message); //show the data
+      message=""; //clear the data
+    }
+  }
+  */
+  Serial.println("Hello World");
+  delay(1000); //delay
+}
+    
