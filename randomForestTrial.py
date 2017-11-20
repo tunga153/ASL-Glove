4d04b33836c356d8b2c3e5159cf5ab61ae59da24
Brian Amaratunga <a3kila@gmail.com>
2017-11-20 14:59:49 -0500

    Indent fixes

--- randomForestTrial.py
+++ randomForestTrial.py
@@ -13,87 +13,92 @@ from os import environ, listdir, makedirs
 from os.path import dirname, exists, expanduser, isdir, join, splitext
 
 def main():
-  # Load scikit's random forest classifier library
-  from sklearn.ensemble import RandomForestClassifier
+    # Load scikit's random forest classifier library
+    from sklearn.ensemble import RandomForestClassifier
 
-  # Set random seed
-  np.random.seed()
+    # Set random seed
+    np.random.seed()
 
-  # Create an object called trainingSignals with the training data
-  trainingSignals = loaddata()
+    # Create an object called trainingSignals with the training data
+    trainingSignals = loaddata()
 
-  # Create a dataframe
-  df = pd.DataFrame(trainingSignals.data, columns=trainingSignals.feature_names)
+    # Create a dataframe
+    df = pd.DataFrame(trainingSignals.data, columns=trainingSignals.feature_names)
 
-  #Populate the class names next to the variable data
-  df['letters'] = pd.Categorical.from_codes(trainingSignals.target, trainingSignals.target_names)
+    #Populate the class names next to the variable data
+    df['letters'] = pd.Categorical.from_codes(trainingSignals.target, trainingSignals.target_names)
 
-  #the fun way of test data (uniform dist at 75%)
-  df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+    #the fun way of test data (uniform dist at 75%)
+    df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
 
-  # Create two new dataframes, one with the training rows, one with the test rows
-  train, test = df[df['is_train']==True], df[df['is_train']==False]
-  # Show the number of observations for the test and training dataframes
-  print('Number of observations in the training data:', len(train))
-  print('Number of observations in the test data:',len(test))
+    # Create two new dataframes, one with the training rows, one with the test rows
+    train, test = df[df['is_train']==True], df[df['is_train']==False]
+    # Show the number of observations for the test and training dataframes
+    print('Number of observations in the training data:', len(train))
+    print('Number of observations in the test data:',len(test))
 
-  # Create a list of the feature column's names
-  features = df.columns[:8]
+    # Create a list of the feature column's names
+    features = df.columns[:8]
 
-  #need to integerize the Letter Names of the classes (0-25 A-Z)
-  y = pd.factorize(train['letters'])[0]
+    #need to integerize the Letter Names of the classes (0-25 A-Z)
+    y = pd.factorize(train['letters'])[0]
 
-  # Create a random forest Classifier. By convention, clf means 'Classifier'
-  clf = RandomForestClassifier(n_jobs=4, random_state=0)
+    # Create a random forest Classifier. By convention, clf means 'Classifier'
+    clf = RandomForestClassifier(n_jobs=4, random_state=0)
 
-  # Train the Classifier on the letter data
-  clf.fit(train[features], y)
+    # Train the Classifier on the letter data
+    clf.fit(train[features], y)
 
-  #Predict a character
-  preds = trainingSignals.target_names[clf.predict(test[features])]
+    #Predict a character
+    preds = trainingSignals.target_names[clf.predict(test[features])]
 
-  print(pd.crosstab(test['letters'], preds, rownames=['Actual Letters'], colnames=['Predicted Letters']))
+    print(pd.crosstab(test['letters'], preds, rownames=['Actual Letters'], colnames=['Predicted Letters']))
 
-  #From here on, loop forever reading data from the serial to use as "test" data
-  ser = serial.Serial('/dev/cu.HC-06-DevB', 9600, timeout=0)
-  while 1:
-    temp = ser.readline()
-    tempArr = temp.split(",")
-    tempArr = tempArr[:len(tempArr)-1]
-    if len(tempArr) != 8:
-        time.sleep(1)
-        continue;
-    x = np.array(tempArr)
-    serialData = x.astype(np.float)
-    d = {"Thumb": serialData[0].tolist(), "Index1": serialData[1].tolist(), "Index2": serialData[2].tolist(),
-        "Middle1": serialData[3].tolist(), "Middle2": serialData[4].tolist(), "Ring1": serialData[5].tolist(),
-        "Ring2": serialData[6].tolist(), "pinky": serialData[7].tolist()}
-
-    '''d = {"Thumb": [serialData[0]], "Index1": [serialData[1]], "Index2": [serialData[2]],
-        "Middle1": [serialData[3]], "Middle2": [serialData[4]], "Ring1": [serialData[5]],
-        "Ring2": [serialData[6]], "pinky": [serialData[7]]}
-
-    d = {"Thumb": [float(tempArr[0])], "Index1": [float(tempArr[1])], "Index2": [float(tempArr[2])],
-        "Middle1": [float(tempArr[3])], "Middle2": [float(tempArr[4])], "Ring1": [float(tempArr[5])],
-        "Ring2": [float(tempArr[6])], "pinky": [float(tempArr[7])]}'''
-    dataDF = pd.DataFrame(data=serialData)
-    dataDF = dataDF.transpose()
-    preds = trainingSignals.target_names[clf.predict(dataDF)]
-    print preds[0]
-    time.sleep(0.5)
+    #From here on, loop forever reading data from the serial to use as "test" data
+    while 1:
+        try:
+            ser = serial.Serial('/dev/cu.HC-06-DevB', 9600, timeout=0)
+            break
+        except serial.serialutil.SerialException:
+            time.sleep(1)
+            print "Error connecting... Trying again"
+    while 1:
+        try:
+            temp = ser.readline()
+            tempArr = temp.split(",")
+            tempArr = tempArr[:len(tempArr)-1]
+            if len(tempArr) != 8:
+              time.sleep(0.5)
+              continue;
+            x = np.array(tempArr)
+            serialData = x.astype(np.float)
+            dataDF = pd.DataFrame(data=serialData)
+            dataDF = dataDF.transpose()
+            preds = trainingSignals.target_names[clf.predict(dataDF)]
+            print preds[0]
+            print ""
+            time.sleep(0.5)
+        except ValueError:
+            time.sleep(0.5)
+            continue
+        except serial.serialutil.SerialException:
+            time.sleep(0.5)
+            ser = serial.Serial('/dev/cu.HC-06-DevB', 9600, timeout=0)
+            print "Error connecting... Trying again"
+            continue
 
 def loaddata():
-  """df=pd.read_csv('binary_train.csv', sep=',',header=None)
-  print(df.values)
-  temp = np.hsplit(df.values,np.array([3,6]))
+    """df=pd.read_csv('binary_train.csv', sep=',',header=None)
+    print(df.values)
+    temp = np.hsplit(df.values,np.array([3,6]))
 
-  returnData = {'data': np.array(temp[0]),'targets':temp[1]}
-  print(returnData['data'])
-  print(returnData['targets'])"""
-  fdescr = "ASL glove training data"
-  module_path = dirname(__file__)
-  data, target, target_names = load_data(module_path, 'binary_train.csv')
-  return sklearn.datasets.base.Bunch(data=data, target=target,
+    returnData = {'data': np.array(temp[0]),'targets':temp[1]}
+    print(returnData['data'])
+    print(returnData['targets'])"""
+    fdescr = "ASL glove training data"
+    module_path = dirname(__file__)
+    data, target, target_names = load_data(module_path, 'binary_train.csv')
+    return sklearn.datasets.base.Bunch(data=data, target=target,
                  target_names=target_names,
                  DESCR=fdescr,
                  feature_names=['Thumb', 'Index1', 'Index2', 'Middle1', 'Middle2', 'Ring1', 'Ring2',
